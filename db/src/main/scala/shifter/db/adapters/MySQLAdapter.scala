26e012fef524eb74875461939437a951edc8fd7f
Alexandru Nedelcu <noreply@alexn.org>
2012-11-20 12:00:35 +0200

    Fix

--- db/src/main/scala/shifter/db/adapters/MySQLAdapter.scala
+++ db/src/main/scala/shifter/db/adapters/MySQLAdapter.scala
@@ -1,9 +1,30 @@
 package shifter.db.adapters
 
-import java.sql.Connection
+import shifter.lang.backports._
+import java.sql._
+import java.lang.Class
 
 
 class MySQLAdapter extends DBAdapter("mysql") {
+  override
+  def initConnection(url: String, user: Option[String], password: Option[String], driver: Option[String]): Connection = {
+    Try {
+      getConn(url, user, password)
+    }
+    .recover {
+      // attempt loading driver
+      case ex:SQLException if ex.getSQLState == "08001" =>
+	Class.forName("com.mysql.jdbc.Driver")
+	super.initConnection(url, user, password, driver)
+    }
+    match {
+      case Failure(ex) => 
+        throw ex
+      case Success(conn: Connection) => 
+        conn      
+    }
+  }
+
   override def listTables(conn: Connection): Seq[String] = {
     val sql = "SHOW TABLES"
 

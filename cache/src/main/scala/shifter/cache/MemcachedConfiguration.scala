a8b2669e8c7110d0aaea9c57657de4673b91c21f
Alexandru Nedelcu <alex@magnolialabs.com>
2013-03-14 14:54:49 +0200

    Add config options for shifter-cache

--- cache/src/main/scala/shifter/cache/MemcachedConfiguration.scala
+++ cache/src/main/scala/shifter/cache/MemcachedConfiguration.scala
@@ -1,15 +1,23 @@
 package shifter.cache
 
+import concurrent.duration._
+
 case class MemcachedConfiguration(
   addresses: String,
   authentication: Option[MemcachedAuthConfiguration] = None,
   keysPrefix: Option[String] = None,
-  protocol: Protocol.Type = Protocol.Binary
+  protocol: Protocol.Type = Protocol.Binary,
+  failureMode: FailureMode.Value = FailureMode.Retry,
+  operationTimeout: FiniteDuration = 1.second
 )
 
 object Protocol extends Enumeration {
   type Type = Value
   val Binary, Text = Value
+}
+
+object FailureMode extends Enumeration {
+  val Retry, Cancel, Redistribute = Value
 }
 
 case class MemcachedAuthConfiguration(

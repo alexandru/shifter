0547b22b05a90fdd768c7c72247b76586de99a37
Alexandru Nedelcu <alex@magnolialabs.com>
2013-03-26 22:17:01 +0200

    Async I/O - initial commit

---
+++ core/src/main/scala/shifter/concurrency/internals/ExecutorWrapper.scala
@@ -0,0 +1,11 @@
+package shifter.concurrency.internals
+
+import concurrent.ExecutionContext
+import java.util.concurrent.Executor
+import util.control.NonFatal
+
+class ExecutorWrapper(ec: ExecutionContext) extends Executor {
+  def execute(command: Runnable) {
+    try ec.execute(command) catch { case NonFatal(ex) => ec.reportFailure(ex) }
+  }
+}

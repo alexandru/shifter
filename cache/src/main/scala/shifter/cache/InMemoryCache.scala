e092fa8ba3772f31f4d55a8fc78ee8a3d983ed1e
Alexandru Nedelcu <alex@magnolialabs.com>
2013-04-01 16:16:23 +0300

    Remove STM dependency from Shifter

--- cache/src/main/scala/shifter/cache/InMemoryCache.scala
+++ cache/src/main/scala/shifter/cache/InMemoryCache.scala
@@ -4,7 +4,7 @@ import concurrent.duration.{FiniteDuration, Duration}
 import concurrent.{Future, ExecutionContext}
 import util.Random
 import java.util.concurrent.atomic.AtomicLong
-import concurrent.stm._
+import shifter.concurrency.atomic.Ref
 
 /**
  * Simple and dumb implementation of an in-memory cache.
@@ -31,7 +31,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
   def add[T](key: String, value: T, exp: Duration = defaultExpiry)(implicit ec: ExecutionContext): Future[Boolean] =
     if (value != null)
       Future {
-        val previous = cache.single.getAndTransform { map =>
+        val previous = cache.getAndTransform { map =>
           if (!map.contains(key) || isExpired(map(key)))
             map.updated(key, Elem(
               value = value,
@@ -53,7 +53,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
   def set[T](key: String, value: T, exp: Duration = defaultExpiry)(implicit ec: ExecutionContext): Future[Unit] =
     if (value != null)
       Future {
-        val map = cache.single.transformAndGet { map => map.updated(key, Elem(
+        val map = cache.transformAndGet { map => map.updated(key, Elem(
           value = value,
           expiresTS = System.currentTimeMillis() + millis(exp)
         ))}
@@ -65,13 +65,13 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
 
   def delete(key: String)(implicit ec: ExecutionContext): Future[Boolean] =
     Future {
-      val map = cache.single.getAndTransform(_ - key)
+      val map = cache.getAndTransform(_ - key)
       cleanup(map.size)
       map.contains(key)
     }
 
   def apply[T](key: String)(implicit ec: ExecutionContext): Future[T] =
-    cache.single.get.get(key) match {
+    cache.get.get(key) match {
       case Some(elem) if !isExpired(elem) =>
         Future.successful(elem.value.asInstanceOf[T])
       case _ =>
@@ -79,7 +79,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
     }
 
   def get[T](key: String)(implicit ec: ExecutionContext): Future[Option[T]] =
-    cache.single.get.get(key) match {
+    cache.get.get(key) match {
       case Some(elem) if !isExpired(elem) =>
         Future.successful(Some(elem.value.asInstanceOf[T]))
       case _ =>
@@ -87,7 +87,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
     }
 
   def getOrElse[T](key: String, default: => T)(implicit ec: ExecutionContext): Future[T] =
-    cache.single.get.get(key) match {
+    cache.get.get(key) match {
       case Some(elem) if !isExpired(elem) =>
         Future.successful(elem.value.asInstanceOf[T])
       case _ =>
@@ -96,7 +96,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
 
   def getBulk(keys: Traversable[String])(implicit ec: ExecutionContext): Future[Map[String, Any]] = {
     val keysSet = keys.toSet
-    val result = cache.single.get.collect {
+    val result = cache.get.collect {
       case (key, value) if keysSet(key) && !isExpired(value) =>
         (key, value.value)
     }
@@ -109,7 +109,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
       def hasElement(map: Map[String, Elem]) =
         map.get(key).filterNot(isExpired).map(_.value.asInstanceOf[T]) == expecting
 
-      val previous = cache.single.getAndTransform { map =>
+      val previous = cache.getAndTransform { map =>
         if (hasElement(map))
           map.updated(key, Elem(
             value = newValue,
@@ -125,7 +125,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
 
   def transformAndGet[T](key: String, exp: Duration)(cb: (Option[T]) => T)(implicit ec: ExecutionContext): Future[T] =
     Future {
-      val next = cache.single.transformAndGet { map =>
+      val next = cache.transformAndGet { map =>
         val expecting = map.get(key).filterNot(isExpired).map(_.value.asInstanceOf[T])
         val result = cb(expecting)
 
@@ -141,7 +141,7 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
 
   def getAndTransform[T](key: String, exp: Duration)(cb: (Option[T]) => T)(implicit ec: ExecutionContext): Future[Option[T]] =
     Future {
-      val prev = cache.single.getAndTransform { map =>
+      val prev = cache.getAndTransform { map =>
         val expecting = map.get(key).filterNot(isExpired).map(_.value.asInstanceOf[T])
         val result = cb(expecting)
 
@@ -157,14 +157,14 @@ class InMemoryCache(maxElems: Int = 5000) extends Cache {
 
 
   def shutdown() {
-    cache.single.transform(current => Map.empty)
+    cache.transform(current => Map.empty)
   }
 
   private[this] def cleanup(size: Int) {
     if (size > maxElems || System.currentTimeMillis() - lastCleanup.get() > 10000) {
       lastCleanup.set(System.currentTimeMillis())
 
-      cache.single.transform {
+      cache.transform {
         map =>
           val filtered = map.filter(elem => !isExpired(elem._2))
           Random.shuffle(filtered).take(maxElems).toMap

740771147b22407a882f7b1bf0ce04f602febdfd
Alexandru Nedelcu <alex@magnolialabs.com>
2013-04-08 17:06:09 +0300

    Fixes; version 0.3.71-SNAPSHOT

--- core/src/main/scala/shifter/io/AsyncFileOutputStream.scala
+++ core/src/main/scala/shifter/io/AsyncFileOutputStream.scala
@@ -1,6 +1,6 @@
 package shifter.io
 
-import scala.concurrent.{Promise, Future, ExecutionContext}
+import scala.concurrent.{Promise, Future}
 import java.io.File
 import java.nio.file.{StandardOpenOption => o}
 import java.nio.ByteBuffer
@@ -8,8 +8,10 @@ import shifter.concurrency.atomic.Ref
 import scala.util.Try
 import scala.concurrent.duration.Duration
 import shifter.concurrency.extensions._
+import shifter.io.Implicits.IOContext
 
-final class AsyncFileOutputStream(file: File)(implicit ec: ExecutionContext) extends AsyncOutputStream {
+
+final class AsyncFileOutputStream(file: File) extends AsyncOutputStream {
   def asyncWrite(b: ByteBuffer): Future[Int] = {
     val length = b.limit() - b.position()
     val buffer: ByteBuffer = if (b.isReadOnly)

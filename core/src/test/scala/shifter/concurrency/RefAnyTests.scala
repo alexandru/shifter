dd73d99d88423685942ee21e2abc80cc1497d023
Stefan Mihaila <stefan@epigrams.co>
2013-04-22 17:11:54 +0300

    Add tests for shifter.concurrency.atomic.Ref

---
+++ core/src/test/scala/shifter/concurrency/RefAnyTests.scala
@@ -0,0 +1,89 @@
+package shifter.concurrency
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import shifter.concurrency.atomic.Ref
+
+@RunWith(classOf[JUnitRunner])
+class RefAnyTests extends FunSuite {
+
+  test("get returns correct value") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+
+    assert(r.get === initialValue)
+  }
+
+  test("set updates value correctly") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+    val updatedValue = "NewValue"
+    r.set(updatedValue)
+
+    assert(r.get === updatedValue)
+  }
+
+  test("lazySet") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+    val updatedValue = "NewValue"
+    r.lazySet(updatedValue)
+
+    assert(r.get === updatedValue)
+  }
+
+  test("getAndSet updates value and returns previous one") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+
+    val updatedValue = new String("NewValue")
+    val result = r.getAndSet(updatedValue)
+
+    assert(result === initialValue)
+    assert(r.get === updatedValue)
+  }
+
+  test("compareAndSet changes value on condition met") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+    val updatedValue = new String("NewValue")
+
+    val success = r.compareAndSet(initialValue, updatedValue)
+
+    assert(r.get === updatedValue)
+    assert(success)
+  }
+
+  test("compareAndSet - value stays the same on condition not met") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+
+    val success = r.compareAndSet(new String("OtherValue"), new String("NewValue"))
+
+    assert(r.get === initialValue)
+    assert(!success)
+  }
+
+  test("weakCompareAndSet changes value on condition met") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+    val updatedValue = new String("NewValue")
+
+    val success = r.weakCompareAndSet(initialValue, updatedValue)
+
+    assert(r.get === updatedValue)
+    assert(success)
+  }
+
+  test("weakCompareAndSet - value stays the same on condition not met") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+
+    val success = r.weakCompareAndSet(new String("OtherValue"), new String("NewValue"))
+
+    assert(r.get === initialValue)
+    assert(!success)
+  }
+
+}
---
+++ core/src/test/scala/shifter/concurrency/RefBooleanTests.scala
@@ -0,0 +1,91 @@
+package shifter.concurrency
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import shifter.concurrency.atomic.Ref
+
+@RunWith(classOf[JUnitRunner])
+class RefBooleanTests extends FunSuite {
+
+  test("get returns correct value") {
+    val falseRef = Ref(false)
+    val trueRef = Ref(true)
+
+    assert(trueRef.get === true)
+    assert(falseRef.get === false)
+  }
+
+  test("set updates value correctly") {
+    val initialValue = false
+    val r = Ref(initialValue)
+    val updatedValue = !initialValue
+    r.set(updatedValue)
+
+    assert(r.get === updatedValue)
+  }
+
+  test("lazySet") {
+    val initialValue = false
+    val r = Ref(initialValue)
+    val updatedValue = !initialValue
+    r.lazySet(updatedValue)
+
+    assert(r.get === updatedValue)
+  }
+
+  test("getAndSet updates value and returns previous one") {
+    val initialValue = false
+    val r = Ref(initialValue)
+
+    val updatedValue = true
+    val result = r.getAndSet(updatedValue)
+
+    assert(result === initialValue)
+    assert(r.get === updatedValue)
+  }
+
+  test("compareAndSet changes value on condition met") {
+    val initialValue = false
+    val r = Ref(initialValue)
+    val updatedValue = !initialValue
+
+    val success = r.compareAndSet(initialValue, updatedValue)
+
+    assert(r.get === updatedValue)
+    assert(success)
+  }
+
+  test("compareAndSet - value stays the same on condition not met") {
+    val initialValue = false
+    val r = Ref(initialValue)
+
+    val success = r.compareAndSet(!initialValue, !initialValue)
+
+    assert(r.get === initialValue)
+    assert(!success)
+  }
+
+  test("weakCompareAndSet changes value on condition met") {
+    val initialValue = true
+    val r = Ref(initialValue)
+    val updatedValue = !initialValue
+
+    val success = r.weakCompareAndSet(initialValue, updatedValue)
+
+    assert(r.get === updatedValue)
+    assert(success === true)
+
+  }
+
+  test("weakCompareAndSet - value stays the same on condition not met") {
+    val initialValue = new String("InitialValue")
+    val r = Ref(initialValue)
+
+    val success = r.weakCompareAndSet(new String("OtherValue"), new String("NewValue"))
+
+    assert(r.get === initialValue)
+    assert(!success)
+  }
+
+}
